4b20c25e7042304ba2f808155ceebab40b4aaadb
martinFRANCOISE14 <martinfrancoise111@outlook.fr>
2025-07-27 10:28:47 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,19 @@
-const CACHE_NAME = "budget-cache-v1";
+const CACHE_NAME = 'budget-cache-v1';
 const urlsToCache = [
   '/',
   '/index.html',
   '/app.js',
-  '/manifest.json'
+  '/manifest.json',
+  '/icon.png'
 ];
 
-self.addEventListener("install", event => {
+self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener("fetch", event => {
+self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(response => response || fetch(event.request))
   );
